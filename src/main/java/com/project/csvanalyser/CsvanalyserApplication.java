043a62f44c14287d36224eaac45f163f3f721bfe
Auroshree Lenka <aurolenka2001@gmail.com>
2026-02-20 23:23:43 +0530

    Initial Commit

---
+++ src/main/java/com/project/csvanalyser/CsvanalyserApplication.java
@@ -0,0 +1,13 @@
+package com.project.csvanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvanalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvanalyserApplication.class, args);
+	}
+
+}
